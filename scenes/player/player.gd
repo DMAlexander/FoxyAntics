8d22af72e6be423afd7e42d69cc561e37b4260ce
Devin Alexander <devinmalexander@gmail.com>
2025-01-10 11:26:08 -0600

    Player jump, Player Debug Label, Player State

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,15 +3,20 @@ extends CharacterBody2D
 class_name Player
 
 
+enum PlayerState { IDLE, RUN, JUMP, FALL, HURT }
+
+
 const GRAVITY: float = 690.0
 const RUN_SPEED: float = 120.0
 const MAX_FALL: float = 400.0
 const JUMP_VELOCITY: float = -260.0
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var debug_label: Label = $DebugLabel
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
-
+var _state: PlayerState = PlayerState.IDLE
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,6 +30,17 @@ func _physics_process(delta: float) -> void:
 		
 	get_input()
 	move_and_slide()
+	calculate_states()
+	update_debug_label()
+
+
+func update_debug_label() -> void:
+	debug_label.text = "floor:%s\n%s\nvel:(%.0f,%.0f)" % [
+		is_on_floor(),
+		PlayerState.keys()[_state],
+		velocity.x, 
+		velocity.y
+	]
 
 
 func get_input() -> void:
@@ -37,3 +53,41 @@ func get_input() -> void:
 	elif Input.is_action_pressed("right") == true:
 		velocity.x = RUN_SPEED
 		sprite_2d.flip_h = false
+			
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL)
+
+func set_state(new_state: PlayerState) -> void:
+	
+	if new_state == _state:
+		return
+		
+	_state = new_state
+	
+	match _state:
+		PlayerState.IDLE:
+			animation_player.play("idle")
+		PlayerState.RUN:
+			animation_player.play("run")
+		PlayerState.JUMP:
+			animation_player.play("jump")
+		PlayerState.FALL:
+			animation_player.play("fall")
+
+func calculate_states() -> void:
+	if is_on_floor() == true:
+		if velocity.x == 0:
+			set_state(PlayerState.IDLE)
+		else:
+			set_state(PlayerState.RUN)
+	else:
+		if velocity.y > 0:
+			set_state(PlayerState.FALL)
+		else:
+			set_state(PlayerState.JUMP)
+
+
+
+	
